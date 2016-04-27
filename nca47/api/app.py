e8c6c63957ba7e7f8a69d60aa3ab707f01496210
zhangyang <zhangyang8@egfbank.com.cn>
2016-04-27 10:55:39 +0800

    Add context hook for nca.
    Add context hook for nca, add pycharm .idea folder to gitignore.

--- nca47/api/app.py
+++ nca47/api/app.py
@@ -1,6 +1,7 @@
 import pecan
 
 from nca47.api import config
+from nca47.api import hooks
 
 
 def get_pecan_config():
@@ -12,13 +13,16 @@ def get_pecan_config():
 
 
 def setup_app(pecan_config=None):
+    app_hooks = [hooks.ContextHook(pecan_config.app.acl_public_routes)]
+
     if not pecan_config:
         pecan_config = get_pecan_config()
 
     pecan.configuration.set_config(dict(pecan_config), overwrite=True)
 
     app = pecan.make_app(
-        pecan_config.app.root
+        pecan_config.app.root,
+        hooks=app_hooks,
     )
 
     return app
---
+++ nca47/api/hooks/__init__.py
@@ -0,0 +1,62 @@
+from pecan import hooks
+
+from nca47.common import context
+
+class ContextHook(hooks.PecanHook):
+    """Configures a request context and attaches it to the request.
+
+    The following HTTP request headers are used:
+
+    X-User-Id or X-User:
+        Used for context.user_id.
+
+    X-Tenant-Id or X-Tenant:
+        Used for context.tenant.
+
+    X-Auth-Token:
+        Used for context.auth_token.
+
+    X-Roles:
+        Used for setting context.is_admin flag to either True or False.
+        The flag is set to True, if X-Roles contains either an administrator
+        or admin substring. Otherwise it is set to False.
+
+    """
+
+    def __init__(self, public_api_routes):
+        self.public_api_routes = public_api_routes
+        super(ContextHook, self).__init__()
+
+    def before(self, state):
+        headers = state.request.headers
+
+        # Do not pass any token with context for noauth mode
+        auth_token = headers.get('X-Auth-Token')
+
+        creds = {
+            'user': headers.get('X-User') or headers.get('X-User-Id'),
+            'tenant': headers.get('X-Tenant') or headers.get('X-Tenant-Id'),
+            'domain_id': headers.get('X-User-Domain-Id'),
+            'domain_name': headers.get('X-User-Domain-Name'),
+            'auth_token': auth_token,
+            'roles': headers.get('X-Roles', '').split(','),
+        }
+
+        # is_admin = policy.enforce('admin_api', creds, creds)
+        is_public_api = state.request.environ.get('is_public_api', False)
+        # show_password = policy.enforce('show_password', creds, creds)
+
+        state.request.context = context.RequestContext(
+            # is_admin=is_admin,
+            is_public_api=is_public_api,
+            # show_password=show_password,
+            **creds)
+
+    def after(self, state):
+        if state.request.context == {}:
+            # An incorrect url path will not create RequestContext
+            return
+        # NOTE(lintan): RequestContext will generate a request_id if no one
+        # passing outside, so it always contain a request_id.
+        request_id = state.request.context.request_id
+        state.response.headers['Openstack-Request-Id'] = request_id
